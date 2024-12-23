9176c4824e829f0b4f043eab69ce9bbe83a1ea07
Luke Wagner <lukedwagner01@gmail.com>
2024-12-22 23:28:06 -0800

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -10,11 +10,15 @@ CREATE TABLE `fens` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `game_data` (
+  `client_name` varchar(45) DEFAULT NULL,
+  `client_email` varchar(45) DEFAULT NULL,
+  `player_name` varchar(45) NOT NULL,
   `game_link` varchar(60) NOT NULL,
   `player_color` varchar(1) DEFAULT NULL,
   `outcome` tinyint(1) DEFAULT NULL,
   `result` varchar(25) DEFAULT NULL,
   `month` char(2) DEFAULT NULL,
   `year` char(4) DEFAULT NULL,
-  PRIMARY KEY (`game_link`)
+  PRIMARY KEY (`player_name`,`game_link`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
