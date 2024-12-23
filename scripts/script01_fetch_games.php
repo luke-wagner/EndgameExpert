a8147a8e91310d1b78231233fb44b63d74dbe085
Luke Wagner <lukedwagner01@gmail.com>
2024-12-23 00:16:49 -0800

    Decide python installation based on os

--- scripts/script01_fetch_games.php
+++ scripts/script01_fetch_games.php
@@ -1,4 +1,14 @@
 <?php
+    // Determine the operating system and set the Python executable
+    // DigitalOcean droplet will run in a virtual environment, so must provide explicit path
+    // to this binary
+    if (PHP_OS_FAMILY === 'Linux') {
+        $python_exec = '/var/www/venv/bin/python3';
+    } else {
+        // Assume Windows as the fallback
+        $python_exec = 'python3';
+    }
+
     $filename = basename(__FILE__, '.php'); 
     $python_script = realpath(dirname(__FILE__) . '/../python/fetch_games.py');
     $outfilepath = realpath(dirname(__FILE__) . '/../out') . '/' . $filename . '.log';    
@@ -9,7 +19,7 @@
     $end_date = escapeshellarg($_GET['end-date']);
 
     // Define the command to call the Python script with arguments
-    $command1 = "python3 $python_script $ccom_username $start_date $end_date 2>&1 1> $outfilepath";
+    $command1 = "$python_exec $python_script $ccom_username $start_date $end_date 2>&1 1> $outfilepath";
 
     // Execute the command and capture the output
     $output1 = shell_exec($command1);
