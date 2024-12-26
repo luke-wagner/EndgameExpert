8f41d97e4a19e8a0ac0829ad99d5fc137bfeae3d
Luke Wagner <lukedwagner01@gmail.com>
2024-12-26 11:27:56 -0800

    Update submodule and write to error file for pgnToFen failures

--- python/categorize_game.py
+++ python/categorize_game.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import re
 import sys
+from datetime import datetime
 
 sys.path.append('../') # to include python files in the root directory
 
@@ -8,6 +9,8 @@ import dependencies.pgnToFen.pgntofen as ptf
 from categorize_pos import *
 
 from config import DB_USERNAME, DB_PWD
+
+ERR_FILE_PATH = '../out/categorize_game.err'
 
 db = mysql.connector.connect(
   host="localhost",
@@ -18,6 +21,8 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
+# Gets all fens for a game (from a pgn)
+# Categorizes and inserts into fens table
 def categorize_game(pgn, player_color, game_link):
     pgnConverter = ptf.PgnToFen()
     pgnConverter.resetBoard()
@@ -51,3 +56,15 @@ def categorize_game(pgn, player_color, game_link):
                 move_number += 1
             
             last_fen = fen
+
+    # When pgn to fen conversion fails, write errors to out file
+    for game_data in stats['failed']:
+        timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        timestamp = '( ' + timestamp + ' )\n'
+        file_lines = [timestamp, game_link + '\n']
+        for elem in game_data:
+            file_lines.append(str(elem) + '\n')
+        file_lines.append('\n\n')
+
+        with open(ERR_FILE_PATH, 'a') as f:
+            f.writelines(file_lines)
